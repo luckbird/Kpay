df9d39e635ae24ffbf2b18fc178d59839f3cf8d6
YST <YST@ysyfdeMac-mini.local>
2017-03-10 16:17:48 +0800

    first init

---
+++ KPayTest/KPayTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KPayTest
+//
+//  Created by YST on 17/3/10.
+//  Copyright © 2017年 YSTSDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KPayTest/KPayTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KPayTest
+//
+//  Created by YST on 17/3/10.
+//  Copyright © 2017年 YSTSDK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
